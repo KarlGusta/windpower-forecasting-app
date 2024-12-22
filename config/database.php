42685e3095b3723d809c4ab695b6381013e66f0b
Karlgusta Esimit <karlgustaesimit@gmail.com>
2024-12-22 13:23:55 +0300

    Added database configuration

---
+++ config/database.php
@@ -0,0 +1,32 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        $config = require 'config.php';
+        $db = $config['database'];
+
+        $this->connection = new mysqli(
+            $db['host'],
+            $db['user'],
+            $db['pass'],
+            $db['name']
+        );
+
+        if ($this->connection->connect_error) {
+            throw new Exception("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
